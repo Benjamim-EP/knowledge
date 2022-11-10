97a29bfb15a9f5f6658228c2eaff3d057a690f97
Benjamim-EP <bannk18@gmail.com>
2022-11-10 16:16:26 -0300

    rota-admin

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+            sub="Cadastros & Cia" />
+        
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+//import ArticleAdmin from './ArticleAdmin'
+//import CategoryAdmin from './CategoryAdmin'
+//import UserAdmin from './UserAdmin'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle}//, ArticleAdmin, CategoryAdmin, UserAdmin }
+}
+</script>
+
+<style>
+
+</style>
