47fb8f16a2a971afb7da9905922683fdec1fb1e8
Benjamim-EP <bannk18@gmail.com>
2022-11-12 14:40:41 -0300

    categorias

--- src/components/admin/CategoryAdmin.vue
+++ src/components/admin/CategoryAdmin.vue
@@ -1,16 +1,49 @@
 <template>
     <div class="category-admin">
-        <h1>Category componente</h1>
+        <b-form>
+            <input id="category-id" type="hidden" v-model="category.id" />
+            <b-form-group label="Nome:" label-for="category-name">
+                <b-form-input id="category-name" type="text"
+                    v-model="category.name" required
+                    :readonly="mode === 'remove'"
+                    placeholder="Informe o Nome da Categoria..." />
+            </b-form-group>
+            <b-form-group label="Categoria Pai:" label-for="category-parentId">
+                <b-form-select v-if="mode === 'save'"
+                    id="category-parentId"
+                    :options="categories" v-model="category.parentId" />
+                <b-form-input v-else
+                    id="category-parentId" type="text"
+                    v-model="category.path"
+                    readonly />
+            </b-form-group>
+            <b-button variant="primary" v-if="mode === 'save'"
+                @click="save">Salvar</b-button>
+            <b-button variant="danger" v-if="mode === 'remove'"
+                @click="remove">Excluir</b-button>
+            <b-button class="ml-2" @click="reset">Cancelar</b-button>
+        </b-form>
+        <hr>
+        <b-table hover striped :items="categories" :fields="fields">
+            <template slot="actions" slot-scope="data">
+                <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-button>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
-//import { baseApiUrl, showError } from '@/global'
-//import axios from 'axios'
+import { baseApiUrl, showError } from '@/global'
+import axios from 'axios'
 
 export default {
     name: 'CategoryAdmin',
-    /*data: function() {
+    data: function() {
         return {
             mode: 'save',
             category: {},
@@ -64,7 +97,7 @@ export default {
     },
     mounted() {
         this.loadCategories()
-    }*/
+    }
 }
 </script>
 
