a7a0c4e89ad663b32f337c8c117e2436241c93be
Benjamim-EP <bannk18@gmail.com>
2022-11-12 18:47:54 -0300

    artigos

--- src/config/router.js
+++ src/config/router.js
@@ -3,8 +3,8 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
-//import ArticlesByCategory from '@/components/article/ArticlesByCategory'
-//import ArticleById from '@/components/article/ArticleById'
+import ArticlesByCategory from '@/components/article/ArticlesByCategory'
+import ArticleById from '@/components/article/ArticleById'
 //import Auth from '@/components/auth/Auth'
 
 //import { userKey } from '@/global'
@@ -20,7 +20,7 @@ const routes = [{
     path: '/admin',
     component: AdminPages,
     meta: { requiresAdmin: true }
-}]/*, {
+}, {
     name: 'articlesByCategory',
     path: '/categories/:id/articles',
     component: ArticlesByCategory
@@ -28,12 +28,12 @@ const routes = [{
     name: 'articleById',
     path: '/articles/:id',
     component: ArticleById
-}, {
+}]/*, {
     name: 'auth',
     path: '/auth',
     component: Auth
-}]*/
-
+}]
+*/
 const router = new VueRouter({
     mode: 'history',
     routes
